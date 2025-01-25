1f4f3236117eb81f050ff7c073be792fb5b9d0b1
DuganChandler <duganchandler10@gmail.com>
2025-01-25 17:19:47 -0500

    added bubble buoyancy

--- Bubble Boy Breaks Out/Assets/Scripts/Player/PlayerController.cs
+++ Bubble Boy Breaks Out/Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 
@@ -16,11 +15,20 @@ public class PlayerController : MonoBehaviour
 
     Vector2 moveInput;
 
+    [Tooltip("How big the bubble is relative to its 'normal' size")]
+    public float bubbleSize = 1.0f; // 1.0 = default size
+    [Tooltip("A multiplier for buoyant force. Increase for more float.")]
+    public float buoyancyFactor = 5f;
+    [Tooltip("Should the bubble also reduce gravity scale? (Optional)")]
+    public float bubbleGravityScale = 0.2f;
 
     public float movementSpeed = 5f;
-    public float jumpPluse = 4f;
+    public float jumpPluse = 1.0f;
+
+    private float bubbleControl = 0f;
 
     public bool CanMove = true;
+
 
     private bool _isFacingRight = true;
     public bool IsFacingRight {
@@ -33,6 +41,16 @@ public class PlayerController : MonoBehaviour
             _isFacingRight = value;
         }
     }
+
+    private bool _isBubble = false;
+    public bool IsBubble {
+        get {
+            return _isBubble;
+        } private set {
+            _isBubble = value;
+            // set animator to bubble boy
+        }
+    }
     
     public float CurrentSpeed { get {
         if (CanMove) {
@@ -41,6 +59,7 @@ public class PlayerController : MonoBehaviour
             return 0;
         }
     }}
+
 
 
     void Awake() {
@@ -57,12 +76,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsBubble) {
+            if (bubbleControl > 0) {
+                bubbleSize += Time.deltaTime * 0.5f;
+            } else if (bubbleControl < 0) {
+                bubbleSize -= Time.deltaTime * 0.5f;
+            }
+            bubbleSize = Mathf.Clamp(bubbleSize, 0.5f, 2f);
+        }
     }
 
     void FixedUpdate() {
-        rb.velocity = new Vector2(moveInput.x * CurrentSpeed, rb.velocity.y);
+        // Normal horizontal movement, but maybe slower if in bubble:
+        float effectiveSpeed = IsBubble ? CurrentSpeed * 0.25f : CurrentSpeed;
+        rb.velocity = new Vector2(moveInput.x * effectiveSpeed, rb.velocity.y);
+
+        if (IsBubble)
+        {
+            // Optionally reduce gravity scale so we don't fall so fast:
+            rb.gravityScale = bubbleGravityScale;
+
+            // Calculate buoyant force based on bubbleSize relative to 1.0 = default
+            // Example: If bubbleSize > 1, we get an upward force
+            // If bubbleSize < 1, maybe no buoyancy or even a small downward force
+            float sizeRatio = bubbleSize - 1.0f; // negative if smaller than default
+            float buoyantForce = sizeRatio * buoyancyFactor;
+
+            // If you only want buoyancy when the bubble is larger than default:
+            if (buoyantForce < 0) buoyantForce = 0;
+
+            // Apply upward force
+            rb.AddForce(Vector2.up * buoyantForce, ForceMode2D.Force);
+
+            // OPTIONAL: Add some linear drag for floaty, slower movement
+            rb.drag = 2f;  
+        }
+        else
+        {
+            // Normal mode
+            rb.gravityScale = 1.0f; 
+            rb.drag = 0f;
+        }
+        // rb.velocity = new Vector2(moveInput.x * CurrentSpeed, rb.velocity.y);
+        // if (IsBubble) {
+        // }
     }
+
+
 
     private bool isOnGround() {
         RaycastHit2D hit = Physics2D.CircleCast(circleCollider.bounds.center, circleCollider.radius, Vector2.down, 0.1f, groundLayer);
@@ -83,8 +143,26 @@ public class PlayerController : MonoBehaviour
     }
 
     public void onJump(InputAction.CallbackContext context) {
-        if (context.started && isOnGround() && CanMove) {
+        Debug.Log(isOnGround());
+        if (context.started && isOnGround() && CanMove && !IsBubble) {
             rb.velocity = new Vector2(rb.velocity.x, jumpPluse);
+        }
+    }
+
+    public void onBubble(InputAction.CallbackContext context) {
+        if (context.started && !IsBubble) {
+            IsBubble = true;
+            Debug.Log("Bubble");
+        } else if (context.started && IsBubble) {
+            IsBubble = false;
+            bubbleSize = 1.0f;
+            Debug.Log("Not Bubble");
+        }
+    }
+
+    public void onBubbleControl(InputAction.CallbackContext context) {
+        if (IsBubble) {
+            bubbleControl = context.ReadValue<Single>();
         }
     }
 }
